3aa013b775a379c8135f9bebc8e78235fcecb60e
Lindsay Mergy <dev.linzxe@gmail.com>
2022-07-07 15:19:19 -0700

    feat: completed automated tests

--- cypress/e2e/product_list.cy.js
+++ cypress/e2e/product_list.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('the home page', () => {
+  it('loads initially', () => {
+    cy.visit('https://lindsaymergy.github.io/apple_acres/')
+    cy.contains('p', 'Loading').should('be.visible')
+  })
+  
+  it('loads phone number', () => {
+    cy.contains('h2', '287-382-4789').should('be.visible')
+  })
+
+  it('loads product listings', () => {
+    cy.get('.product').should('have.length', 8)
+  })
+
+  it('loads product details', () => {
+    cy.get('.details').first().click()
+    cy.get('.description').should('be.visible')
+    cy.get('.rating').should('be.visible')
   })
 })
